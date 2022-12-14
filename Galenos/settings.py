827d8e1e26d14735cb05812e67117c8e40d8a50c
Seba Galdames Vial <se.galdames@duocuc.cl>
2022-12-14 20:13:38 -0300

    oracledatabase

--- Galenos/settings.py
+++ Galenos/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'Galenos',
 ]
 
 MIDDLEWARE = [
@@ -76,10 +77,18 @@ WSGI_APPLICATION = 'Galenos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/xe',
+        'USER': 'c##galenos',
+        'PASSWORD': 'galenos',
+        'TEST':{
+            'USER' : 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp'
+        }
+}}
+
+
 
 
 # Password validation
--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Agenda (models.Model):
+    idAgenda = models.AutoField(primary_key=True)
+    dia = models.DateField()
+    hora = models.CharField(max_length=6)
+    rutmedico = models.CharField(max_length=20)
+    
+
+    def __str__(self):
+        return self.nome
