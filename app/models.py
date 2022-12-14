ae30e41cca4578a511742f150cfb086bbe70f587
Seba Galdames Vial <se.galdames@duocuc.cl>
2022-12-14 20:28:48 -0300

    allmodels

--- app/models.py
+++ app/models.py
@@ -9,6 +9,62 @@ class Agenda (models.Model):
     hora = models.CharField(max_length=6)
     rutmedico = models.CharField(max_length=20)
     
+class Cajero (models.Model):
+    rut = models.CharField(primary_key=True, max_length=20)
+    rol = models.CharField(max_length=20)
+    
+class CalendarioBase (models.Model):
+    idCalendarioBase = models.AutoField(primary_key=True)
+    fecha = models.DateField()
+
+class HoraMedica (models.Model):
+    idhoraMedica = models.AutoField(primary_key=True)
+    Hora = models.CharField(max_length=6)
+    dia= models.DateField()
+    rut= models.CharField(max_length=20)
+    
+class Informe (models.Model):
+    idInforme = models.AutoField(primary_key=True)
+    fecha = models.DateField()
+    contenido = models.CharField(max_length=900)
+
+class Medico (models.Model):
+    rut = models.CharField(primary_key=True, max_length=20)
+    rol = models.CharField(max_length=20)
+    especialidad = models.CharField(max_length=20)
+
+class Paciente (models.Model):
+    rut= models.CharField(primary_key=True, max_length=20)
+    nombre= models.CharField(max_length=20)
+    apellido= models.CharField(max_length=20)
+    fechaNacimiento= models.DateField()
+    correo= models.CharField(max_length=50)
+    direccion= models.CharField(max_length=50)
+    celular= models.IntegerField()
+    
+class Pago (models.Model):
+    idPago= models.AutoField(primary_key=True)
+    monto= models.IntegerField()
+    rut= models.CharField(max_length=20)
+
+class Secretaria (models.Model):
+    rol= models.CharField(max_length=20)
+    rut= models.CharField(primary_key=True, max_length=20)
+
+class Trabajador (models.Model):
+    rut= models.CharField(primary_key=True, max_length=20)
+    nombre= models.CharField(max_length=20)
+    apellido= models.CharField(max_length=20)
+    fechaNacimiento= models.DateField()
+    correo= models.CharField(max_length=50)
+    direccion= models.CharField(max_length=50)
+    telefono= models.IntegerField()
+    rol= models.CharField(max_length=20)
+
+
+
 
     def __str__(self):
         return self.nome
+    
+    
