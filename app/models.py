f9c99c7fb2bbbeecfb26670d616dcd18eec19054
Seba Galdames Vial <se.galdames@duocuc.cl>
2022-12-15 03:28:01 -0300

    hora paciente

--- app/models.py
+++ app/models.py
@@ -23,6 +23,7 @@ class HoraMedica (models.Model):
     dia= models.DateField()
     rut= models.CharField(max_length=20)
     
+    
 class Informe (models.Model):
     idInforme = models.AutoField(primary_key=True)
     fecha = models.DateField()
@@ -62,7 +63,18 @@ class Trabajador (models.Model):
     rol= models.CharField(max_length=20)
 
 
-
+class horaPaciente (models.Model):
+    rut= models.CharField(primary_key=True, max_length=20)
+    nombre = models.CharField(max_length=50)
+    apellidoPaterno = models.CharField(max_length=50)
+    apellidoMaterno = models.CharField(max_length=50)
+    correo= models.CharField(max_length=50)
+    direccion= models.CharField(max_length=50)
+    telefono= models.IntegerField()
+    especialidad= models.CharField(max_length=50)
+    fecha= models.DateField()
+    hora= models.CharField(max_length=6)
+    
 
     def __str__(self):
         return self.nome
