fba3d15da5db6525017d69d166d14d32bab31354
Seba Galdames Vial <se.galdames@duocuc.cl>
2022-12-14 23:34:43 -0300

    procedimientos 1

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.db import connection
+import cx_Oracle
 
 
 # Create your views here.
@@ -32,7 +33,26 @@ def test(request):
     data = {
         'pacientes': lista_pacientes(),  
     }
+    
+    
+    if request.method == 'POST':
+        rut = request.POST.get('rut')
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        fechanacimiento = request.POST.get('fecha_nacimiento')
+        correo= request.POST.get('correo')
+        direccion= request.POST.get('direccion')
+        celular= request.POST.get('celular')
+        salida=agregarPaciente(rut,nombre,apellido,fechanacimiento,correo,direccion,celular)
+        if salida == 1:
+            data['mensaje'] = 'Paciente agregado correctamente'
+            data['pacientes'] = lista_pacientes()
+        else:
+            data['mensaje'] = 'Error al agregar paciente'
+        
+        
     return render(request, 'app/test.html',data)
+    
     
 #---------------------------------------------------------------
 #PROCEDIMIENTOS ALMACENADOS
@@ -49,5 +69,12 @@ def lista_pacientes():
         listaPaci.append(fila)
         
     return listaPaci
-    
-    
+
+
+def agregarPaciente(rut,nombre,apellido,fechanacimiento,correo,direccion,celular):
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor() #llama
+    salida = cursor.var(cx_Oracle.NUMBER)
+    cursor.callproc('sp_agr_paciente', [rut,nombre,apellido,fechanacimiento,correo,direccion,celular,salida])
+    return salida.getvalue()
+  
